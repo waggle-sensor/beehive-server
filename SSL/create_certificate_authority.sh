01d3543f71ab0665add3ee1776b546d949510230
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2015-11-17 21:55:04 -0600

    split cert stuff

---
+++ SSL/create_certificate_authority.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+export SSL_DIR="/usr/lib/waggle/SSL"
+
+# Begin constructing the Certificate Authority
+cd ${SSL_DIR}/waggleca
+
+# Make appropriate folders
+mkdir -p certs private
+chmod 700 private
+echo 01 > serial
+touch index.txt
+
+# Generate the root certificate
+
+openssl req -x509 -config openssl.cnf -newkey rsa:2048 -days 365 \
+	-out cacert.pem -outform PEM -subj /CN=waggleca/ -nodes
+
+openssl x509 -in cacert.pem -out cacert.cer -outform DER
+
+
---
+++ SSL/create_server_cert.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+export SSL_DIR="/usr/lib/waggle/SSL"
+
+
+# Make the server certificate
+
+cd ${SSL_DIR} # in SSL/
+
+mkdir -p server
+chmod 744 server
+cd ${SSL_DIR}/server
+
+openssl genrsa -out key.pem 2048
+
+openssl req -new -key key.pem -out req.pem -outform PEM \
+	-subj /CN=$(hostname)/O=server/ -nodes
+
+cd ${SSL_DIR}/waggleca
+
+openssl ca -config openssl.cnf -in ${SSL_DIR}/server/req.pem -out \
+	${SSL_DIR}/server/cert.pem -notext -batch -extensions server_ca_extensions
+
+cd ${SSL_DIR}/server
+
+openssl pkcs12 -export -out keycert.p12 -in cert.pem -inkey key.pem -passout pass:waggle
+
