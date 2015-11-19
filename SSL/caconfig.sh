559b28b3167e1134439963a41d741084300b433c
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2015-11-19 14:13:56 -0600

    fix path

--- SSL/caconfig.sh
+++ SSL/caconfig.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-./SSL/create_certificate_authority.sh
+./create_certificate_authority.sh
 
-./SSL/create_server_cert.sh
+./create_server_cert.sh
 
 # Copy files to correct places
 cp ${SSL_DIR}/rabbitmq.config /etc/rabbitmq/
