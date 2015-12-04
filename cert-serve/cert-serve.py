0d9ef733d933c7259e29fb7ff0e0e14b8f0f57ee
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2015-12-04 10:58:32 -0600

    cert by nodeid

--- cert-serve/cert-serve.py
+++ cert-serve/cert-serve.py
@@ -3,13 +3,21 @@ import web
 import os.path
 import subprocess
 import threading
+import re
 
-
+# docker run -ti -p 9999:9999 -v ${DATA}/waggle/SSL/:/usr/lib/waggle/SSL/ --name cert-serve waggle/beehive-server /bin/bash
+# cd /usr/lib/waggle/beehive-server/cert-serve ; ./cert-serve.py
 # apt-get install python-webpy
 # pip install web.py
-
+# echo "unique_subject = no" > SSL/waggleca/index.txt.attr
 
 resource_lock = threading.RLock()
+
+ssl_path = "/usr/lib/waggle/beehive-server/SSL/"
+
+hexaPattern = re.compile(r'^([0-9A-F]+)$')
+prog = re.compile(hexaPattern)
+
 
 def read_file( str ):
     if not os.path.isfile(str) :
@@ -24,35 +32,60 @@ def read_file( str ):
 
 urls = (
     '/certca', 'certca',
-    '/newnode', 'newnode',
-    '/(.*)', 'use resources /certca and /newnode'
+    '/newnode(?.*)', 'newnode',
+    '/', 'index'
 )
 
 app = web.application(urls, globals())
 
-class hello:        
-    def GET(self, name):
-        if not name: 
-            name = 'world'
-        return 'Hello, ' + name + '!'
+class index:        
+    def GET(self):
+        return 'This is the Waggle certificate server.'
 
 class certca:        
     def GET(self):
-        result = read_file("/usr/lib/waggle/SSL/waggleca/cacert.pem")
+        result = read_file(ssl_path + "waggleca/cacert.pem")
         if not result:
             return "error: cacert file not found !?"
 
         return result
 
 class newnode:        
-    def GET(self):
+    def GET(self, nodeid):
 
+        # TODO: make it an option to allow or disallow anonymous nodes
+        if nodeid:
+            print "requested cert wit nodeid"
+            if nodeid.startswith("?"):
+                nodeid = nodeid[1:].upper()
+                
+                result = prog.match(nodeid)
+                if not result:
+                    return "error: Could not parse node id."
+                    
+                # TODO check that this is a valid node id
+            else
+                return "error: Could not parse argument. Questionmark missing."
+        else:
+            print "requested cert without nodeid"
+        
+        
         privkey=""
         cert=""
-        with resource_lock:
-            subprocess.call(['/usr/lib/waggle/beehive-server/SSL/create_client_cert.sh', 'node', 'temp_client_cert'])
-            privkey = read_file("/usr/lib/waggle/SSL/temp_client_cert/key.pem")
-            cert = read_file("/usr/lib/waggle/SSL/temp_client_cert/cert.pem")
+        
+        if nodeid:
+            node_dir = ssl_path + 'node_'+ nodeid
+            if not os.path.isdir():
+                with resource_lock:
+                    subprocess.call([ssl_path + 'create_client_cert.sh', 'node', 'node_'+ nodeid])
+            
+            privkey = read_file(ssl_path + 'node_'+ nodeid + '/key.pem')
+            cert    = read_file(ssl_path + 'node_'+ nodeid + '/cert.pem')       
+        else:
+            with resource_lock:
+                subprocess.call([ssl_path + 'create_client_cert.sh', 'node', 'temp_client_cert'])
+                privkey = read_file(ssl_path + 'temp_client_cert/key.pem')
+                cert    = read_file(ssl_path + 'temp_client_cert/cert.pem')
             
         if not privkey:
             return "error: privkey file not found !?"
@@ -60,6 +93,10 @@ class newnode:
         if not cert:
             return "error: cert file not found !?"
 
+        if nodeid:
+            print "issuing cert for node "+nodeid
+        else:
+            print "issuing cert for unknown node"
         return privkey + "\n" + cert
 
 
@@ -67,7 +104,6 @@ if __name__ == "__main__":
     web.httpserver.runsimple(app.wsgifunc(), ("0.0.0.0", 9999))
     app.run()
 
-# docker run -ti -p 9999:9999 -v ${DATA}/waggle/SSL/:/usr/lib/waggle/SSL/ --name cert-serve waggle/beehive-server /bin/bash
-# cd /usr/lib/waggle/beehive-server/cert-serve ; ./cert-serve.py
+
 
 
