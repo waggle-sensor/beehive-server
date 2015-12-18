2325520206a1174261eeceaca51d94e6bf06a5d6
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2015-12-17 18:41:08 -0600

    Update cert-serve.py

--- cert-serve/cert-serve.py
+++ cert-serve/cert-serve.py
@@ -5,11 +5,7 @@ import subprocess
 import threading
 import re
 
-# docker run -ti -p 9999:9999 -v ${DATA}/waggle/SSL/:/usr/lib/waggle/SSL/ --name cert-serve waggle/beehive-server /bin/bash
-# cd /usr/lib/waggle/beehive-server/cert-serve ; ./cert-serve.py
-# apt-get install python-webpy
-# pip install web.py
-# echo "unique_subject = no" > SSL/waggleca/index.txt.attr
+
 
 resource_lock = threading.RLock()
 
