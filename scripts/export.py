3675e359afcf08ead45cb212158f23e5cef9fc8f
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2016-01-22 11:28:10 -0600

    export ttl and normal data

--- scripts/export.py
+++ scripts/export.py
@@ -3,12 +3,15 @@
 import logging, time, argparse
 from cassandra.cluster import Cluster
 
+#start container:
+#docker run -it  -v ${DATA}/export:/export --link beehive-cassandra:cassandra --rm waggle/beehive-server /bin/bash
+
 
 logger = logging.getLogger(__name__)
 
 CASSANDRA_HOST="cassandra"
 
-def export(node_id, date):
+def export(node_id, date, ttl):
     cluster = Cluster(contact_points=[CASSANDRA_HOST])
     session = None
 
@@ -22,7 +25,15 @@ def export(node_id, date):
         if not session:
             time.sleep(3)
 
-    statement = "SELECT node_id, date, plugin_id, plugin_version, plugin_instance, timestamp, sensor, sensor_meta, data FROM waggle.sensor_data_ttl WHERE node_id='%s' AND date='%s'" %(node_id, date)
+    if not ttl:
+        statement = "SELECT node_id, date, plugin_id, plugin_version, plugin_instance, timestamp, sensor, sensor_meta, data "+
+                    "FROM waggle.sensor_data "+
+                    "WHERE node_id='%s' AND date='%s'" %(node_id, date)
+    else:
+        statement = "SELECT node_id, date, plugin_id, plugin_version, plugin_instance, timestamp, sensor, sensor_meta, data "+
+                    "FROM waggle.sensor_data_ttl "+
+                    "WHERE node_id='%s'" %(node_id)
+
     rows = session.execute(statement)
 
 
@@ -36,12 +47,14 @@ if __name__ == "__main__":
     
     parser = argparse.ArgumentParser()
     #parser.add_argument('--logging', dest='enable_logging', help='write to log files instead of stdout', action='store_true')
-    parser.add_argument('--node_id', dest='node_id', help='node_id', action='store_true')
-    parser.add_argument('--date', dest='date', help='date, format: YYYY-MM-DD, e.g. 2016-01-21', action='store_true')
+    parser.add_argument('ttl', help='export only ttl data (latest sensor data)' action='store_false'
+    parser.add_argument('--node_id', dest='node_id', help='node_id')
+    parser.add_argument('--date', dest='date', help='date (not needed with ttl), format: YYYY-MM-DD, e.g. 2016-01-21')
     
     
     args = parser.parse_args()
     
+    export(args.node_id, args.date, args.ttl)
         
    
 
