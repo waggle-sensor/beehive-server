6e3c35dca58167dfdceb97585b7154f4c4cec776
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2016-02-25 16:34:54 -0600

    fix error message

--- dataprocess.py
+++ dataprocess.py
@@ -111,10 +111,10 @@ class DataProcess(Process):
         try:
             timestamp_int = int(data[4])
         except ValueError as e:
-            logger.error("(ValueError) Error converting timestamp (%s) into int: %s (sender: %s)" % (data[3], str(e), s_uniqid_str))
+            logger.error("(ValueError) Error converting timestamp (%s) into int: %s (sender: %s)" % (data[4], str(e), s_uniqid_str))
             raise
         except Exception as e:
-            logger.error("(Exception) Error converting timestamp (%s) into int: %s (sender: %s)" % (data[3], str(e), s_uniqid_str))
+            logger.error("(Exception) Error converting timestamp (%s) into int: %s (sender: %s)" % (data[4], str(e), s_uniqid_str))
             raise
         
         try:
