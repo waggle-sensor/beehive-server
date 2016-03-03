7086863bc1ca1bdbb1bce4e4008927ef568e1ab0
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2016-03-03 16:02:31 -0600

    draft of sshd for reverse sh tunnel connections

---
+++ beehive-sshd/run_sshd.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+import subprocess
+import sys
+import select
+import time
+
+
+log_forward_prefix = 'debug1: Local forwarding listening on 127.0.0.1 port '
+
+def handle_file_descriptors(ret):
+    
+    for fd in ret[0]:
+        if fd == p.stdout.fileno():
+            read = p.stdout.readline()
+            if read:
+                sys.stdout.write('stdout: ' + read)
+                stdout.append(read)
+        if fd == p.stderr.fileno():
+            read = p.stderr.readline()
+            if read:
+                if read.startswith(log_forward_prefix):
+                    print "MATCH !!!!!!!"
+                    print 'port:', read[len(log_forward_prefix):]
+                sys.stderr.write('stderr: ' + read)
+                stderr.append(read)
+
+
+if __name__ == "__main__":
+
+
+    while True:
+        p = subprocess.Popen(["/usr/sbin/sshd", "-d"],
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        stdout = []
+        stderr = []
+
+        while True:
+            reads = [p.stdout.fileno(), p.stderr.fileno()]
+            ret = select.select(reads, [], [])
+
+            handle_file_descriptors(ret)
+
+            if p.poll() != None:
+                time.sleep(3)
+                ret = select.select(reads, [], [])
+                handle_file_descriptors(ret)
+                break
+
+            print 'sshd ended !?'
+
+            print 'stdout:', "".join(stdout)
+            print 'stderr:', "".join(stderr)
