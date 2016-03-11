b82546d812b785158191b80606c8f5aadb5d3619
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2016-03-11 13:43:36 -0600

    add mysql

---
+++ beehive-mysql/README.md
@@ -0,0 +1,23 @@
+
+
+# Beehive MySQL
+
+
+Start container:
+
+```bash
+docker rm -f beehive-mysql
+
+[ ! -z "$DATA" ] && \
+docker run -d \
+  --name beehive-mysql \
+  --net beehive \
+  -v ${DATA}/mysql/datadir:/var/lib/mysql \
+  -e MYSQL_ROOT_PASSWORD=waggle \
+  -e MYSQL_DATABASE=waggle \
+  -e MYSQL_USER=waggle \
+  -e MYSQL_PASSWORD=waggle \
+  mysql:5.7.10
+```
+
+
---
+++ beehive-mysql/tables.sql
@@ -0,0 +1,39 @@
+
+use waggle;
+
+DROP TABLE users;
+
+CREATE TABLE users (
+    id                  INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username            VARCHAR(64) NOT NULL UNIQUE,
+    password            VARCHAR(64),
+    real_name           VARCHAR(64),
+    email               VARCHAR(64),
+    address             VARCHAR(64),
+    city                VARCHAR(32),
+    postal_code         VARCHAR(10),
+    country             VARCHAR(15),
+    admin               BOOLEAN NOT NULL DEFAULT 0
+);
+
+ALTER TABLE users ADD INDEX (username);
+
+
+CREATE TABLE projects (
+    id                  INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name                VARCHAR(60),
+    owner               INT,
+    public              BOOLEAN NOT NULL DEFAULT 0,
+    ext_link            VARCHAR(256)
+);
+ALTER TABLE projects ADD INDEX (owner);
+
+CREATE TABLE nodes (
+    id                  INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    node_id             VARCHAR(16),
+    project             INT,
+    description         VARCHAR(255),
+    reverse_ssh_port    MEDIUMINT
+);
+ALTER TABLE nodes ADD INDEX (project);
+
