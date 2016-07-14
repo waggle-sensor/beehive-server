b28707c1d8f1d790d5cc222ef16d382c5f6f43a6
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2016-07-14 11:58:58 -0500

    keep string

--- beehive-api/export.py
+++ beehive-api/export.py
@@ -98,8 +98,8 @@ def list_node_dates():
     nodes={}
     count = 0;
     for (node_id, date) in rows:
-        node_id = node_id.encode('ascii','replace').lower()
-        date = date.encode('ascii','replace')
+        node_id = node_id.lower()
+    
         if not node_id in nodes:
             nodes[node_id]=[]
             count = count +1
