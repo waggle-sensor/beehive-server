888a0f9052db41ce35562092e76344124717c9cc
MuffinSpawn <muffinspawn@gmail.com>
2016-08-29 14:05:48 -0500

    Make curl run quietly.

--- beehive-sshd/register.sh
+++ beehive-sshd/register.sh
@@ -4,6 +4,6 @@ cert_server_ip=172.18.0.9
 
 api_call=${SSH_ORIGINAL_COMMAND}
 if [[ $api_call == "certca" || $api_call == "node?"* ]]; then
-  curl "$cert_server_ip/$api_call"
+  curl -s "$cert_server_ip/$api_call"
 fi
 
