6fb5bb8cd0ae9cbde8524a76b3098f860e1dc17d
wcatino <wcatino@gmail.com>
2016-10-18 17:29:49 -0500

    typo

--- data-pipeline/injectTestData.py
+++ data-pipeline/injectTestData.py
@@ -54,8 +54,8 @@ if __name__ == '__main__':
                     headers     = { 
                                     meta_id     : '0',
                                     data_set    : 'testsensor:v1:0',
-                                    unit        : 'unit0'
-                                    parameter   : 'param0',
+                                    unit        : 'unit0',
+                                    parameter   : 'param0'
                     }
             )
         print('properties = ', myProperties)
