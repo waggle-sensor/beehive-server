57acc69e84c76fe0c2acebf48d85be864c1c87ba
wcatino <wcatino@gmail.com>
2016-10-24 17:10:57 -0500

    Move these files that create tables in each of the 2 databases.

---
+++ beehive-cassandra/createTablesCassandra.sql
@@ -0,0 +1,48 @@
+USE waggle;
+
+CREATE TABLE sensor_data_raw (
+    node_id         ascii,    
+    date            ascii,    
+    ingest_id       int,
+    plugin_name     ascii,
+    plugin_version  ascii,
+    plugin_instance ascii,
+    timestamp       TIMESTAMP,      -- milliseconds from epoch, integer
+    parameter       ascii,          -- parameter name (eg. temperature, humidity)
+    data            ascii,          -- data from sensor, encoded to hex
+    PRIMARY KEY  ((node_id, date), plugin_name, plugin_version, plugin_instance, timestamp, parameter)
+);
+
+CREATE TABLE sensor_data_decoded (
+    node_id         ascii,
+    date            ascii,
+    ingest_id       int,
+    meta_id         int,            -- foreign key into node_meta table
+    timestamp       TIMESTAMP,      -- milliseconds from epoch, integer
+    data_set        ascii,          -- distinguish between identical sensors on same node
+    sensor          ascii,          -- eg. TMP112
+    parameter       ascii,          -- parameter name (eg. temperature, humidity)
+    data            ascii,          -- data from sensor, encoded to hex
+    unit            ascii,
+    PRIMARY KEY ((node_id, date), meta_id, sensor, parameter, timestamp, data_set, ingest_id, unit)
+);
+
+CREATE TABLE admin_messages (
+    node_id         ascii,
+    date            ascii,
+    ingest_id       int,
+    meta_id         int,            -- foreign key into node_meta table
+    timestamp       TIMESTAMP,      -- milliseconds from epoch, integer
+    data_set        ascii,          -- distinguish between identical sensors on same node
+    sensor          ascii,    
+    parameter       ascii,          -- parameter name (eg. temperature, humidity)
+    data            ascii,          -- data from sensor, encoded to hex
+    unit            ascii,
+    PRIMARY KEY ((node_id, date), meta_id, sensor, parameter, timestamp, data_set, ingest_id, unit)
+);
+
+CREATE TABLE nodes_last_update (
+    node_id         ascii,
+    last_update     TIMESTAMP,      -- milliseconds from epoch, integer
+    PRIMARY KEY     (node_id)
+);
