8674029e6cde2ded1b0c99c809d0be052bcd58a2
wcatino <wcatino@gmail.com>
2016-11-11 14:29:08 -0600

    typo

--- beehive-web/webserver.py
+++ beehive-web/webserver.py
@@ -112,7 +112,7 @@ class test:
         yield html_header("Waggle Beehive web server - test page.")
         dtUtcNow = datetime.datetime.utcnow()
 
-        yield('UTC now is: {}   =   {} '.format(dtUtcNow))
+        yield 'UTC now is: {}   =   {} \n\n'.format(dtUtcNow)
         yield html_footer()
 
                     
