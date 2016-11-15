c997a112f4f8f0bdca597f5d9f14980bb111e3fc
Sean Shahkarami <sean.shahkarami@gmail.com>
2016-11-15 16:08:52 -0600

    just pulls latest package

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -24,18 +24,19 @@ fi
 # platform-independent python packages
 pip install web.py
 
-cd packages_o/
+# cd packages_o/
 pip install blist
 pip install cassandra-driver
 pip install crcmod
 pip install configparser
 pip install pika
+pip install cqlsh
 
 #cd pika-0.9.14/
 #python setup.py install
 
-cd ../../
-cd packages_o/
+# cd ../../
+# cd packages_o/
 pip3 install blist
 pip3 install cassandra-driver
 pip3 install crcmod
@@ -44,9 +45,7 @@ pip3 install pika
 
 #cd pika-0.10.0/
 #python3 setup.py install
-
-cd ../../
-
+# cd ../../
 # cqlshlib for the cassandra client
-cd cassandra-pylib/
-python ./setup.py install
+# cd cassandra-pylib/
+# python ./setup.py install
