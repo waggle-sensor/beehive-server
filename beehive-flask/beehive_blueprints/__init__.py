6fa3e4f68e3ac0e56c06c4f2669ba7a066780111
wcatino <wcatino@gmail.com>
2016-12-05 16:52:05 -0600

    debug

--- beehive-flask/beehive_blueprints/__init__.py
+++ beehive-flask/beehive_blueprints/__init__.py
@@ -9,5 +9,5 @@ app = Flask(__name__)
 # Puts the API blueprint on api.U2FtIEJsYWNr.com.
 app.register_blueprint(admin, url_prefix = '/admin')
 app.register_blueprint(api,   url_prefix = '/api')
-app.register_blueprint(web,   url_prefix = '/web/')
+app.register_blueprint(web,   url_prefix = '/web')
 
