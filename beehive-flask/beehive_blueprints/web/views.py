fa1444e03b6ea5b2352996fc40b71ad89d09ccc8
wcatino <wcatino@gmail.com>
2016-12-05 16:47:03 -0600

    a test route

--- beehive-flask/beehive_blueprints/web/views.py
+++ beehive-flask/beehive_blueprints/web/views.py
@@ -4,6 +4,25 @@ from . import web
 def web_root():
     return 'You found web!!!'
 
-@web.route('/search/')
-def web_search():
-    return 'found web Search'
+@web.route("/wcc/test/")
+def hello():
+    rl = [] # result list
+
+    rl.append("<h1 style='color:green'>Hello There!</h1>")
+    rl.append('args = ' + str(request.args))
+    rl.append('<br>')
+    d = request.args.to_dict()
+    rl.append('to_dict = ' + str(d))
+    rl.append('<br>')
+    rl.append('<br>')
+    rl.append('items:  ' + str(d.items()))
+    rl.append('<br>')
+    rl.append('<br>')
+    for x in d.keys():
+        rl.append('   {} : {} <br>'.format(x, d[x]))
+    debug = request.args.get('debug', 'False')
+    rl.append('<br>')
+    rl.append('debug :  {}'.format(debug))
+    return ''.join(rl)
+    
+    
