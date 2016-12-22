4389efa804f3f8638d831e6fc700c1b7865122a1
wcatino <wcatino@gmail.com>
2016-12-22 14:42:52 -0600

    tweak test

--- beehive-flask/test.bash
+++ beehive-flask/test.bash
@@ -7,8 +7,7 @@ function url() {
 }
 
 
-if true
-then
+if true; then
     url 0.0.0.0/admin/
     url 0.0.0.0/admin/search/
 
@@ -17,9 +16,11 @@ then
     url 0.0.0.0/api/1/epoch
     url 0.0.0.0/api/1/nodes/
     url 0.0.0.0/api/1/nodes/?all=true
+    url 0.0.0.0/api/nodes
     url 0.0.0.0/api/1/nodes/ffffffffffff0001/dates/?version=2
     url 0.0.0.0/api/nodes/ffffffffffff0001/dates/
     url 0.0.0.0/api/1/nodes_last_update/
+    url 0.0.0.0/api/1/nodes/ffffffffffff0001/export
     url 0.0.0.0/api/1/WCC_node/ffffffffffff0001/
 fi
 
