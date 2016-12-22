d5486eef101430048b27d32dd208e27fccdb9e09
wcatino <wcatino@gmail.com>
2016-12-22 14:23:31 -0600

    tweak test

--- beehive-flask/test.bash
+++ beehive-flask/test.bash
@@ -1,35 +1,33 @@
-
 function url() {
-	echo " "
-	echo "#############" $*
-	echo " "
-	curl $*
-	echo " "
+    echo " "
+    echo "#############" $*
+    echo " "
+    curl $*
+    echo " "
 }
 
 
-if false
+if true
 then
-	url 0.0.0.0/admin/
-	url 0.0.0.0/admin/search/
+    url 0.0.0.0/admin/
+    url 0.0.0.0/admin/search/
 
-	url 0.0.0.0/api/
-	url 0.0.0.0/api/1/
-	url 0.0.0.0/api/1/epoch
-	url 0.0.0.0/api/1/nodes/
-	url 0.0.0.0/api/1/nodes/0000001e061088c8/dates/
-	url 0.0.0.0/api/nodes/0000001e061088c8/dates/
-	url 0.0.0.0/api/1/nodes_last_update/
-	url 0.0.0.0/api/1/WCC_node/0000001e061088c8/
+    url 0.0.0.0/api/
+    url 0.0.0.0/api/1/
+    url 0.0.0.0/api/1/epoch
+    url 0.0.0.0/api/1/nodes/
+    url 0.0.0.0/api/1/nodes/?all=true
+    url 0.0.0.0/api/1/nodes/ffffffffffff0001/dates/?version=2
+    url 0.0.0.0/api/nodes/ffffffffffff0001/dates/
+    url 0.0.0.0/api/1/nodes_last_update/
+    url 0.0.0.0/api/1/WCC_node/ffffffffffff0001/
 fi
 
-url 0.0.0.0/
-url 0.0.0.0/wcc/test/
-url "0.0.0.0/wcc/test/?dog=cat&tiger=lion"
-url 0.0.0.0/web/
-url 0.0.0.0/web/wcc/test/
-url "0.0.0.0/web/wcc/test/?dog=cat&tiger=lion"
-url 0.0.0.0/web/wcc/
-# url 0.0.0.0/web/wcc/node/0000001e061088c8
+if false; then
+    url 0.0.0.0/
+    url 0.0.0.0/wcc/test/
+    url "0.0.0.0/wcc/test/?dog=cat&tiger=lion"
+    url 0.0.0.0/wcc/
+    url 0.0.0.0/web/wcc/node/ffffffffffff0001
+fi
 echo " "
-
