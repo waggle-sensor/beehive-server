2d8a9aeaa455087f360280225fa8bdfcc5009590
wcatino <wcatino@gmail.com>
2016-12-23 19:05:26 -0600

    dev

--- beehive-flask/beehive_blueprints/api/views.py
+++ beehive-flask/beehive_blueprints/api/views.py
@@ -266,7 +266,7 @@ def WCC_nodes_test():
     # if bAllNodes ('b' is for 'bool') is True, print all nodes, otherwise filter the active ones
     bAllNodes = request.args.get('all', 'false').lower() == 'true'
     
-    all_nodes = export.get_nodes(bAllNodes = False)
+    all_nodes = export.get_nodes(bAllNodes)
     
     obj = {}
     obj['data'] = all_nodes
