f7d3bca9c8393a3da99dc021ff9ee560a08488cc
HeyZoos <jbracho@uchicago.edu>
2017-01-12 14:01:54 -0600

    Integrate flask admin on the fly

--- beehive-flask/run.py
+++ beehive-flask/run.py
@@ -1,4 +1,34 @@
-import beehive_blueprints
+from flask import Flask
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.automap import automap_base
 
-beehive_blueprints.app.run(host = '0.0.0.0', port = 5000, debug = True)
+from beehive_blueprints.admin import admin
+from beehive_blueprints.api import api
+from beehive_blueprints.web import web
+
+
+SQLALCHEMY_DATABASE_URI = "mysql://waggle:waggle@172.18.0.4/waggle"
+
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+
+# app.register_blueprint(admin, url_prefix="/admin")
+app.register_blueprint(api, url_prefix="/api")
+app.register_blueprint(web, url_prefix="")
+
+db = SQLAlchemy(app)
+base = automap_base()
+base.prepare(db.engine, reflect=True)
+
+admin = Admin(app, name='beehive-server', template_mode='bootstrap3')
+for table in base.classes:
+    admin.add_view(ModelView(table, db.session))
+
+
+if __name__ == "__main__":
+    
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
