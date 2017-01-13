6572aa5d5f4d52e3f61a130603a751c7607ca4bd
wcatino <wcatino@gmail.com>
2017-01-13 09:27:04 -0600

    Jesse's changes

--- data-pipeline/plenario-sender.py
+++ data-pipeline/plenario-sender.py
@@ -1,23 +1,43 @@
 #!/usr/bin/env python
 
-import sys
-
-sys.path.append('..')
-from config import *
-sys.path.pop()
-
-# import boto3
-from datetime import datetime
+import base64
+import boto3
 import json
 import os
 import pika
-from pprint import pprint
 import re
 import ssl
+
+from datetime import datetime
+from pprint import pprint
 from urllib.parse import urlencode
 
 
+# Make sure these environment variables are in your bashrc!
+# Your keys must have NO SPECIAL CHARACTERS like '\' or '$' because amazon.
+#
+# $ vim ~/.bashrc
+#
+# export AWS_ACCESS_KEY="***ACCESS_KEY***"
+# export AWS_SECRET_KEY="***SECRET_KEY***"
+#
+# Then either refresh your terminal or run source.
+#
+# $ source ~/.bashrc
+
+AWS_ACCESS_KEY = os.environ['AWS_ACCESS_KEY']
+AWS_SECRET_KEY = os.environ['AWS_SECRET_KEY']
+
+kinesis_client = boto3.client(
+    'kinesis',
+    aws_access_key_id=AWS_ACCESS_KEY,
+    aws_secret_access_key=AWS_SECRET_KEY,
+    region_name='us-east-1',
+)
+
+
 def parse_node_list(table):
+
     return set(map(str.strip, table.strip().splitlines()))
 
 
@@ -143,43 +163,16 @@ allowed_nodes = parse_node_list('''
 0000001e0610ba72
 ''')
 
-# setup rabbitmq client
-url = 'amqps://node:waggle@beehive1.mcs.anl.gov:23181?{}'.format(urlencode({
+# url = 'amqps://node:waggle@beehive1.mcs.anl.gov:23181?{}'.format(urlencode({
+url = 'amqps://jbracho:password@0.0.0.0:23181?{}'.format(urlencode({
     'ssl': 't',
     'ssl_options': {
-        'certfile': os.path.abspath('/mnt/waggle/SSL/node/cert.pem'),
-        'keyfile': os.path.abspath('/mnt/waggle/SSL/node/key.pem'),
+        'certfile': os.path.abspath('/mnt/waggle/SSL/beehive-server/cert.pem'),
+        'keyfile': os.path.abspath('/mnt/waggle/SSL/beehive-server/key.pem'),
         'ca_certs': os.path.abspath('/mnt/waggle/SSL/waggleca/cacert.pem'),
         'cert_reqs': ssl.CERT_REQUIRED
     }
 }))
-
-print('url = ', url)
-
-if False:
-    connection = pika.BlockingConnection(pika.URLParameters(url))
-else:
-    connection = pika.BlockingConnection(pika_params)
-
-channel = connection.channel()
-
-channel.exchange_declare(exchange='plugins-out',
-                         exchange_type='fanout',
-                         durable=True)
-
-channel.queue_declare(queue='plenario',
-                      durable=True)
-
-channel.queue_bind(queue='plenario',
-                   exchange='plugins-out')
-
-# setup kinesis client
-kinesis_client = boto3.client(
-    'kinesis',
-    aws_access_key_id='*** SECRET ***',
-    aws_secret_access_key='*** PASSWORD ***',
-    region_name='us-east-1',
-)
 
 
 def map_values(sensor, values):
@@ -189,6 +182,7 @@ def map_values(sensor, values):
 
 
 def callback(ch, method, properties, body):
+
     node_id = properties.reply_to
     sensor = properties.type
     timestamp = datetime.fromtimestamp(properties.timestamp / 1000)
@@ -201,15 +195,35 @@ def callback(ch, method, properties, body):
             'data': dict(map_values(sensor, json.loads(body.decode()))),
             'datetime': timestamp.strftime('%Y-%m-%dT%H:%M:%S'),
         }
-        pprint(payload)
-        print()
 
         kinesis_client.put_record(**{
             'StreamName': 'ObservationStream',
             'PartitionKey': 'arbitrary',
-            'Data': json.dumps(payload)  # body.decode()
+            'Data': json.dumps(payload)
         })
 
+        print("[plenario-sender] Successfully sent: \n")
+        pprint(payload)
+        print()
 
-channel.basic_consume(callback, queue='plenario', no_ack=True)
-channel.start_consuming()
+
+if __name__ == "__main__":
+
+    print("[plenario-sender] Start!")
+
+    print("[plenario-sender] mapping: \n")
+    pprint(mapping)
+    print()
+
+    print("[plenario-sender] allowed_nodes: \n")
+    pprint(allowed_nodes)
+    print()
+
+    connection = pika.BlockingConnection(pika.URLParameters(url))
+
+    channel = connection.channel()
+    channel.exchange_declare(exchange='plugins-out', exchange_type='fanout', durable=True)
+    channel.queue_declare(queue='plenario', durable=True)
+    channel.queue_bind(queue='plenario', exchange='plugins-out')
+    channel.basic_consume(callback, queue='plenario', no_ack=True)
+    channel.start_consuming()
