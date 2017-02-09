b74a75100dc1a7e8727d95525a9cd44d16f4a7a3
wcatino <wcatino@gmail.com>
2017-02-08 21:42:52 -0600

    debug import path

--- data-pipeline/QueueToDb.py
+++ data-pipeline/QueueToDb.py
@@ -5,6 +5,7 @@ import os
 import sys
 sys.path.append(os.path.abspath('../'))
 sys.path.append("/usr/lib/waggle/")
+sys.path.append("/usr/lib/waggle/beehive-server")
 
 import argparse
 import binascii
