46eff89fefe6399f312c03f686d96b2f2b1b318d
wcatino <wcatino@gmail.com>
2017-02-11 16:57:53 -0600

    typo

--- data-pipeline/LastUpdate2.py
+++ data-pipeline/LastUpdate2.py
@@ -207,7 +207,7 @@ if __name__ == '__main__':
     
     setUpdated = set()
     q = Queue(2000)
-    p = LastUpdateProcess(q, mode = args.dataToTrack, verbosity)
+    p = LastUpdateProcess(q, args.dataToTrack, verbosity)
     p.start()
     
     print(__name__ + ': created process ', p)
