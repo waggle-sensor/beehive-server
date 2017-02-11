5c677eecaeccbe6c5a5d022f4c414bdfff484f06
Sean Shahkarami <sean.shahkarami@gmail.com>
2017-02-10 23:34:45 -0600

    rollback data prefix in node api

--- beehive-flask/beehive_blueprints/api/views.py
+++ beehive-flask/beehive_blueprints/api/views.py
@@ -145,7 +145,9 @@ def NodeQuery(node_id_queried=None, bAllNodes=False):
             'last_updated': last_updated
         })
 
-    return dict((node['node_id'], node) for node in all_nodes)
+    return {
+        'data': dict((node['node_id'], node) for node in all_nodes)
+    }
 
     # if bAllNodes and not node_id_queried:
     #     nodes_dict = export.list_node_dates()
