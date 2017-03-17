cc06a17f79f48329187fb4bb8b20737d1965e1f7
wcatino <wcatino@gmail.com>
2017-03-17 15:45:15 -0500

    add service to store log messages to node-specific files

---
+++ data-pipeline/logSaver.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import argparse
+from datetime import datetime
+import pika
+
+
+def callback(ch, method, properties, body):
+    headers = properties.headers
+
+    node_id = properties.reply_to[4:].lower()
+    priority = getpriority(headers['value'])
+
+    print('<{}>{} {}'.format(priority, node_id, body.decode()), flush=True)
+    
+#_______________________________________________________________________
+if __name__ == '__main__':
+
+    # command-line arguments
+    argParser = argparse.ArgumentParser()
+    argParser.add_argument('--verbose', '-v', action = 'count')
+    argParser.add_argument('-debug', action = 'store_true')
+    args = argParser.parse_args()
+    verbosity = 0 if not args.verbose else args.verbose
+    if verbosity: print('args =', args)
+
+    # set up the rabbitmq connection
+    credentials = pika.PlainCredentials('server', 'waggle')
+    parameters = pika.ConnectionParameters('beehive-rabbitmq', credentials=credentials)
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+    
+    channel.queue_declare(queue='log-saver', durable=True)
+    channel.queue_bind(queue='log-saver', exchange='logs', routing_key='#')
+
+    channel.basic_consume(callback, queue='log-saver', no_ack=True)
+    channel.start_consuming()
