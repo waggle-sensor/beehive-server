e12e92300e848fe8d3256ef4b03df9006638b514
wcatino <wcatino@gmail.com>
2017-03-26 04:52:11 -0500

    reduce time between checks, remove debug "echo"

--- monitors/monitor-queues.bash
+++ monitors/monitor-queues.bash
@@ -1,9 +1,9 @@
 while true; do
     queuesTooFull=`docker exec beehive-rabbitmq bash -c "rabbitmqctl list_queues" | grep -v node_ | grep -v Listing | awk '($2 >= 20){print}' `
     if [ $(echo $queuesTooFull | wc -w) -ne 0 ]; then 
-        echo /bin/slack-ops ":exclamation: :ocean: queue(s) filling up:\n\`\`\`\n${queuesTooFull}\n\`\`\`"
+        /bin/slack-ops ":exclamation: :ocean: queue(s) filling up:\n\`\`\`\n${queuesTooFull}\n\`\`\`"
         sleep 300    # extra time - so we don't flood slack
     fi 
-    sleep 60 
+    sleep 30 
 done
 
