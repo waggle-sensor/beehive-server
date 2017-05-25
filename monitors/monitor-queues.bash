cf2745453f37192a7a4484c6bc3a2367bae1173e
wcatino <wcatino@gmail.com>
2017-05-25 11:44:25 -0500

    ignore metrics queue

--- monitors/monitor-queues.bash
+++ monitors/monitor-queues.bash
@@ -1,5 +1,5 @@
 while true; do
-    queuesTooFull=`docker exec beehive-rabbitmq bash -c "rabbitmqctl list_queues" | grep -v node_ | grep -v Listing | awk '($2 >= 80){print}' `
+    queuesTooFull=`docker exec beehive-rabbitmq bash -c "rabbitmqctl list_queues" | grep -v node_ | grep -v Listing | grep -v metrics | awk '($2 >= 80){print}' `
     if [ $(echo $queuesTooFull | wc -w) -ne 0 ]; then 
         /bin/slack-ops ":exclamation: :ocean: queue(s) filling up:\n\`\`\`\n${queuesTooFull}\n\`\`\`"
         sleep 300    # extra time - so we don't flood slack
