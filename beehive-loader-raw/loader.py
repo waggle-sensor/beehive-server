57f6c3ae44f0621e093406e7e6f471da74e4c04d
Sean Shahkarami <sean.shahkarami@gmail.com>
2017-09-09 21:31:48 -0500

    porting queue-to-db-raw into loader

--- beehive-loader-raw/loader.py
+++ beehive-loader-raw/loader.py
@@ -1,66 +1,48 @@
-from cassandra.cqlengine import columns
-from cassandra.cqlengine import connection
-from cassandra.cqlengine.management import sync_table
-from cassandra.cqlengine.models import Model
+from cassandra.cluster import Cluster
 from datetime import datetime
 import pika
 import os
+import binascii
 
 BEEHIVE_DEPLOYMENT = os.environ.get('BEEHIVE_DEPLOYMENT', 'development')
 
 
-class SensorData(Model):
-
-    node_id = columns.Text(partition_key=True)
-    date = columns.Date(partition_key=True)
-    created_at = columns.DateTime(primary_key=True)
-    received_at = columns.DateTime()
-    plugin_id = columns.Text(primary_key=True)
-    topic = columns.Text(primary_key=True)
-    data = columns.Blob()
+cluster = Cluster(contact_points=['beehive-cassandra'])
+session = cluster.connect('waggle')
+query = 'INSERT INTO sensor_data_raw (node_id, date, plugin_name, plugin_version, plugin_instance, timestamp, parameter, data) VALUES (?, ?, ?, ?, ?, ?, ?, ?)'
+prepared = session.prepare(query)
 
 
 def process_message(ch, method, properties, body):
-    user_id = properties.user_id
-    received_at = datetime.utcnow()
-    created_at = datetime.utcfromtimestamp(properties.timestamp // 1000)
-    plugin_id = method.routing_key
-    topic = properties.type
+    versionStrings = properties.app_id.split(':')
+    sampleDatetime = datetime.datetime.utcfromtimestamp(float(properties.timestamp) / 1000.0)
+    sampleDate = sampleDatetime.strftime('%Y-%m-%d')
+    # TODO Validate / santize node_id here.
+    node_id = properties.reply_to
+    plugin_name = versionStrings[0]
+    plugin_version = versionStrings[1]
+    plugin_instance = '0' if (len(versionStrings) < 3) else versionStrings[2]
+    timestamp = int(properties.timestamp)
+    parameter = properties.type
+    data = binascii.hexlify(body).decode()
 
-    SensorData.create(
-        node_id=user_id,
-        date=received_at.date(),
-        created_at=created_at,
-        received_at=received_at,
-        plugin_id=plugin_id,
-        topic=topic,
-        data=body)
+    session.execute(prepared, (node_id, sampleDate, plugin_name, plugin_version, plugin_instance, timestamp, parameter, data))
 
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
-if __name__ == '__main__':
-    # setup cassandra connection and models
-    connection.setup(['beehive-cassandra'], BEEHIVE_DEPLOYMENT)
-    sync_table(SensorData)
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+    host='beehive-rabbitmq',
+    port=5672,
+    virtual_host=BEEHIVE_DEPLOYMENT,
+    credentials=pika.PlainCredentials(
+        username='loader_raw',
+        password='waggle',
+    ),
+    connection_attempts=10,
+    retry_delay=3.0))
 
-    # connect to message broker
-    connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='beehive-rabbitmq',
-        port=5672,
-        virtual_host=BEEHIVE_DEPLOYMENT,
-        credentials=pika.PlainCredentials(
-            username='loader_raw',
-            password='waggle',
-        ),
-        connection_attempts=10,
-        retry_delay=3.0))
-
-    # get channel and start processing messages
-    channel = connection.channel()
-
-    channel.basic_consume(
-        process_message,
-        queue='raw-data')
-
-    channel.start_consuming()
+channel = connection.channel()
+# channel.basic_qos(prefetch_count=1)
+channel.basic_consume(process_message, queue='raw-data')
+channel.start_consuming()
