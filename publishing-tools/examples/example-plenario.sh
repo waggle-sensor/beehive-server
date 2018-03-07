bbfe30f0ab33ca6d9f2da4c6f3c0893b8b037e32
Sean Shahkarami <sean.shahkarami@gmail.com>
2018-03-07 09:07:44 -0600

    updated plenario example to use new metadata

--- publishing-tools/examples/example-plenario.sh
+++ publishing-tools/examples/example-plenario.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 # will be done on beehive
-cat recent.csv |
-../bin/filter-view view.json |
-../bin/filter-sensors climate.json > plenario.csv
+../bin/filter-view plenario < recent.csv |
+../bin/filter-sensors climate.json | tee plenario.csv
 
 # may be done on beehive or at endpoint
-./prepare-for-plenario view.json < plenario.csv
+#./prepare-for-plenario view.json < plenario.csv
