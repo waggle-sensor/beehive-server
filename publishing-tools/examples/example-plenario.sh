cc60926d7ff56a4dec0dfa02031f82e7bebd940f
Sean Shahkarami <sean.shahkarami@gmail.com>
2018-03-07 16:08:17 -0600

    made pipe one line

--- publishing-tools/examples/example-plenario.sh
+++ publishing-tools/examples/example-plenario.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 # will be done on beehive
-../bin/filter-view plenario < recent.csv |
-../bin/filter-sensors climate.csv > plenario.csv
+../bin/filter-view plenario < recent.csv | ../bin/filter-sensors climate.csv > plenario.csv
 
 # may be done on beehive or at endpoint
 ../bin/prepare-for-plenario plenario < plenario.csv
