7ccf8855ededb8140c7fbcfc328183d4655a27c6
Sean Shahkarami <sean.shahkarami@gmail.com>
2018-03-07 16:12:55 -0600

    changed note

--- publishing-tools/examples/example-plenario.sh
+++ publishing-tools/examples/example-plenario.sh
@@ -3,5 +3,5 @@
 # will be done on beehive
 ../bin/filter-view plenario < recent.csv | ../bin/filter-sensors climate.csv > plenario.csv
 
-# may be done on beehive or at endpoint
+# may be done on beehive or by consumer
 ../bin/prepare-for-plenario plenario < plenario.csv
