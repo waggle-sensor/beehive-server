b554f10eb61305ff549aeae6653541a3c38b5dd3
Sean Shahkarami <sean.shahkarami@gmail.com>
2018-06-14 14:37:39 -0500

    ca generation now works

--- beehive-cert/SSL/create_certificate_authority.sh
+++ beehive-cert/SSL/create_certificate_authority.sh
@@ -3,46 +3,62 @@
 SSL_DIR="/usr/lib/waggle/SSL"
 CA_DIR="${SSL_DIR}/waggleca"
 
-mkdir -p $CA_DIR
-#chmod 700 $SSL_DIR
-chmod 700 $CA_DIR
+create_ca_dir() {
+	mkdir -p $CA_DIR
+	chmod 700 $CA_DIR
+	cd $CA_DIR
 
-cd $CA_DIR
+	# Make appropriate folders
+	mkdir -p certs private
+	chmod 700 private
 
-# Make appropriate folders
-mkdir -p certs private
-chmod 700 private
+	if [ ! -f serial ]; then
+		echo 01 > serial
+	fi
 
-if [ ! -f serial ]; then
-	echo 01 > serial
-fi
+	touch index.txt
 
-touch index.txt
+	# this is needed for "node" certificates. We may change that later.
+	echo "unique_subject = no" > index.txt.attr
 
-# this is needed for "node" certificates. We may change that later.
-echo "unique_subject = no" > index.txt.attr
+	cp /usr/lib/waggle/beehive-server/beehive-cert/SSL/waggleca/openssl.cnf openssl.cnf
+}
 
-# Generate the root certificate
+create_ca_key_if_needed() {
+	cd $CA_DIR
 
-if [ -f "$CA_DIR/private/cakey.pem" ]; then
-	echo "CA key already exists. Will use pre-existing key."
-else
-	openssl genrsa -out $CA_DIR/private/cakey.pem 2048
-	rm $CA_DIR/cacert.pem
-fi
+	if [ -f "private/cakey.pem" ]; then
+		echo "CA key already exists."
+	else
+		echo "Creating CA key."
+		openssl genrsa -out $CA_DIR/private/cakey.pem 2048
+		rm -f $CA_DIR/cacert.pem
+		rm $CA_DIR/certs/*
+	fi
+}
 
-if [ -f "$CA_DIR/cacert.pem" ]; then
-	echo "Error: CA cert already exists."
-else
-	openssl req \
-		-new \
-		-x509 \
-		-config /usr/lib/waggle/beehive-server/beehive-cert/SSL/waggleca \
-		-key $CA_DIR/private/cakey.pem \
-		-days 3650 \
-		-out cacert.pem \
-		-outform PEM \
-		-subj /CN=waggleca/
+create_ca_cert_if_needed() {
+	cd $CA_DIR
 
-	# openssl x509 -in cacert.pem -out cacert.cer -outform DER
-fi
+	if [ -f "cacert.pem" ]; then
+		echo "CA certificate already exists."
+	else
+		echo "Creating CA certificate."
+
+		openssl req \
+			-new \
+			-x509 \
+			-key private/cakey.pem \
+			-days 3650 \
+			-out cacert.pem \
+			-outform PEM \
+			-subj /CN=waggleca/ \
+			-sha256
+			# openssl req -new -x509 -key private/cakey.pem -days 3650 -out cacert.pem -outform PEM -subj /CN=waggleca/ -sha256
+			# openssl x509 -in cacert.pem -out cacert.cer -outform DER
+	fi
+}
+
+create_ca_dir
+create_ca_key_if_needed
+create_ca_cert_if_needed
