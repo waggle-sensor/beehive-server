601d88145f81352f88a673d1a65021ac2f4c2d7f
Sean Shahkarami <sean.shahkarami@gmail.com>
2018-06-15 10:13:50 -0500

    simplifying deployment

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-for image in $(echo beehive-*); do
-  cd $image
-  make build
-  cd ..
-done
--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-for image in $(echo beehive-*); do
-  cd $image
-  make deploy
-  cd ..
-done
