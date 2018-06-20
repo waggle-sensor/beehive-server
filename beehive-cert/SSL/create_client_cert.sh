013d2efc422debdca653fa0fb9df1a51fd9dad81
Sean Shahkarami <sean.shahkarami@gmail.com>
2018-06-20 16:13:30 -0500

    allow client cert creation process to incrementally rebuild client data. allows us to *reuse a node's private key but renew it's certificate*.

--- beehive-cert/SSL/create_client_cert.sh
+++ beehive-cert/SSL/create_client_cert.sh
@@ -7,8 +7,8 @@ set -e
 export SSL_DIR="/usr/lib/waggle/SSL"
 
 if [ ! $# -eq 2 ];  then
-    echo "usage: ./create_client_cert.sh node|server <dirname>"
-    echo " example: ./create_client_cert.sh node node1"
+    echo "usage: ./create_client_cert.sh commonname <dirname>"
+    echo " example: ./create_client_cert.sh node123 node123"
     exit 1
 fi
 
@@ -22,24 +22,31 @@ mkdir -p ${SSL_DIR}/${CERT_DIR}
 
 # create key
 cd ${SSL_DIR}/${CERT_DIR}
-openssl genrsa -out key.pem 2048
 
-chmod 600 key.pem
+if [ -f "key.pem" ]; then
+  echo "Client private key already exists."
+else
+  echo "Creating client private key."
+  openssl genrsa -out key.pem 2048
+  chmod 600 key.pem
+fi
 
-# create public RSA to allow node to create reverse ssh tunnel
-# -y          : Read private key file and print public key.
-# -f filename : Filename of the key file.
-ssh-keygen -y -f key.pem > key_rsa.pub.tmp
-mv key_rsa.pub.tmp key_rsa.pub
+# extract public key from private key
+ssh-keygen -y -f key.pem > key_rsa.pub
 
-# create certificate request
-openssl req -new -key ./key.pem -out req.pem -outform PEM \
-    -subj /CN=${RABBIT_USER}/O=client/ -nodes
+if [ -f "cert.pem" ]; then
+  echo "Client certificate already exists."
+else
+  echo "Creating client certificate."
+  # create certificate request
+  openssl req -new -key ./key.pem -out req.pem -outform PEM \
+      -subj /CN=${RABBIT_USER}/O=client/ -nodes
 
-cd ${SSL_DIR}/waggleca
+  cd ${SSL_DIR}/waggleca
 
-# sign
-openssl ca -config openssl.cnf -in ${SSL_DIR}/${CERT_DIR}/req.pem -out  ${SSL_DIR}/${CERT_DIR}/cert.pem -notext -batch -extensions client_ca_extensions
+  # sign
+  openssl ca -config openssl.cnf -in ${SSL_DIR}/${CERT_DIR}/req.pem -out  ${SSL_DIR}/${CERT_DIR}/cert.pem -notext -batch -extensions client_ca_extensions
 
-# pkcs12 , might be needed for java clients for example. Not sure if we actually need that.
-openssl pkcs12 -export -out ${SSL_DIR}/${CERT_DIR}/keycert.p12 -in ${SSL_DIR}/${CERT_DIR}/cert.pem -inkey ${SSL_DIR}/${CERT_DIR}/key.pem -passout pass:waggle
+  # pkcs12 , might be needed for java clients for example. Not sure if we actually need that.
+  openssl pkcs12 -export -out ${SSL_DIR}/${CERT_DIR}/keycert.p12 -in ${SSL_DIR}/${CERT_DIR}/cert.pem -inkey ${SSL_DIR}/${CERT_DIR}/key.pem -passout pass:waggle
+fi
