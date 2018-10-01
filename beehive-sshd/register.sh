7629759b4bd808bd5de4ecee475682bb848fefdf
Sean Shahkarami <sean.shahkarami@gmail.com>
2018-10-01 10:47:43 -0500

    cleaned up register script

--- beehive-sshd/register.sh
+++ beehive-sshd/register.sh
@@ -5,10 +5,10 @@ cert_server_ip=beehive-cert
 run_command() {
   case $1 in
     cacert)
-      curl -s "$cert_server_ip/$1"
+      curl -s "$cert_server_ip/certca"
       ;;
     certca)
-      curl -s "$cert_server_ip/$1"
+      curl -s "$cert_server_ip/certca"
       ;;
     node?*)
       curl -s "$cert_server_ip/$1"
