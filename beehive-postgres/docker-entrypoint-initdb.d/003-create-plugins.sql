55a265276bcea6f7ebef16a45d9903f84306bade
Vince Forgione <vforgione@uchicago.edu>
2019-05-28 14:37:21 -0500

    Added plugin id for packets

--- beehive-postgres/docker-entrypoint-initdb.d/003-create-plugins.sql
+++ beehive-postgres/docker-entrypoint-initdb.d/003-create-plugins.sql
@@ -4,6 +4,8 @@
 CREATE TABLE IF NOT EXISTS plugins (
   -- auto incrementing database id
   id SERIAL PRIMARY KEY,
+  -- the id used in the packets -- a stand in for the name
+  packet_id TEXT NOT NULL UNIQUE,
   -- the name of the plugin
   name TEXT NOT NULL,
   -- the version of the plugin
