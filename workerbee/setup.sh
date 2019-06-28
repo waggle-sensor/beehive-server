2027f8bf2435f9c29cc561feccf91d0a4fa57253
Sean Shahkarami <sean.shahkarami@gmail.com>
2019-06-28 11:14:54 -0500

    added clone

--- workerbee/setup.sh
+++ workerbee/setup.sh
@@ -3,5 +3,9 @@
 basedir=$(pwd $(dirname $0))
 
 cd $basedir
+
+git clone https://github.com/waggle-sensor/beehive-server
+git clone https://github.com/waggle-sensor/plugin_manager
+
 python3 -m venv waggle-python
 source waggle-python/bin/activate && pip install -r requirements.txt
