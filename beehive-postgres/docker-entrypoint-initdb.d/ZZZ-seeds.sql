a0f0ef5c618f5a722c5161fd7fb9bd12f453cd9c
Vince Forgione <vforgione@uchicago.edu>
2019-07-01 11:37:54 -0500

    Fixed table name

--- beehive-postgres/docker-entrypoint-initdb.d/ZZZ-seeds.sql
+++ beehive-postgres/docker-entrypoint-initdb.d/ZZZ-seeds.sql
@@ -2,11 +2,11 @@
 INSERT INTO nodes (node_id, vsn)
     VALUES ('0000000000000001', '001');
 
-INSERT INTO plugins (id, packet_id, version, name) 
+INSERT INTO plugins (id, packet_id, version, name)
     VALUES (9999, '1', '0.0.1', 'TestSense');
 
 INSERT INTO sensors (id, packet_sensor_id, packet_parameter_id, sensor, parameter, uom)
     VALUES (9999, '1', '1', 'uptime', 'uptime', 'seconds');
 
-INSERT INTO plugin_sensors (plugin_id, sensor_id, ontology, subsystem)
+INSERT INTO plugins_sensors (plugin_id, sensor_id, ontology, subsystem)
     VALUES (9999, 9999, '/system/uptime', 'nc');
