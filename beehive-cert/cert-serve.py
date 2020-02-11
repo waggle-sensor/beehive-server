105538f95d50381aa0de0555f7cc6864d9627fe1
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2020-02-11 15:24:03 -0600

    load node credentials into mysql

--- beehive-cert/cert-serve.py
+++ beehive-cert/cert-serve.py
@@ -21,6 +21,10 @@ from os.path import isdir, join
 from mysql import Mysql
 import json
 
+# uses web.py
+# see https://webpy.org/
+
+
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - line=%(lineno)d - %(message)s')
 
 handler = logging.StreamHandler(stream=sys.stdout)
@@ -31,6 +35,14 @@ logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
 
 
+
+mysql_host = os.environ['MYSQL_HOST']
+mysql_user = os.environ['MYSQL_USER']
+mysql_passwd = os.environ['MYSQL_PASSWD']
+mysql_db = os.environ['MYSQL_DB']
+
+
+
 def ensure_dirs(path):
     try:
         os.makedirs(path)
@@ -38,9 +50,9 @@ def ensure_dirs(path):
         pass
 
 
-def read_file(path):
-    with open(path, 'r') as file:
-        return file.read()
+#def read_file(path):
+#    with open(path, 'r') as file:
+#        return file.read()
 
 
 httpserver_port = 80
@@ -99,7 +111,7 @@ def generate_token_from_key_and_cert(key, cert):
     hexdigest = hashlib.sha1(data).hexdigest()
     return hexdigest[:8]
 
-def generate_credentials(nodeid):
+def generate_credentials(db, nodeid):
 
 
         node_dir = os.path.join(ssl_nodes_dir, 'node_' + nodeid)
@@ -112,7 +124,7 @@ def generate_credentials(nodeid):
         signed_client_certificate_file=os.path.join(node_dir, 'cert.pem')
 
 
-        rsa_public_key = None
+        rsa_public_key = ""
 
         with resource_lock:
             return_value = subprocess.call([
@@ -121,41 +133,26 @@ def generate_credentials(nodeid):
                 os.path.join('nodes/', 'node_' + nodeid),  # BUG create_client_cert.sh already prefixes path...
             ])
             if return_value != 0:
-                raise Exception("create_client_cert.sh failed"
-
+                raise Exception("create_client_cert.sh failed")
+            
+            
             rsa_public_key = read_file(rsa_public_key_file)
             append_to_authorized_keys_file(rsa_public_key)
 
 
         rsa_private_key = read_file(rsa_private_key_file)
         signed_client_certificate = read_file(signed_client_certificate_file)
-        #key_rsa_pub_file_content = read_file(key_rsa_pub_file)
+        #rsa_public_key = read_file(rsa_public_key_file)
 
-        db = Mysql( host="beehive-mysql",
-                        user="waggle",
-                        passwd="waggle",
-                        db="waggle")
+        
 
        
 
         
 
-        mysql_row_node = db.get_node(nodeid)
+        
 
-        if not mysql_row_node:
-            port=db.createNewNode(nodeid)
-            if not port:
-                print "Error: Node creation failed"
-                raise Exception("Node creation failed")
-            mysql_row_node = db.get_node(nodeid)
-
-        port = int(db.find_port(nodeid))
-
-        if not port:
-            logger.error("Error: port number not found !?")
-            raise Exception("port number not found !?"
-
-        token = generate_token_from_key_and_cert(key=rsa_private_key, cert=signed_client_certificate)
+        #token = generate_token_from_key_and_cert(key=rsa_private_key, cert=signed_client_certificate)
         
         # TODO: decide if we keep token
          
@@ -183,32 +180,77 @@ class newnode:
 
         logger.info('GET newnode - Preparing to register "%s".', nodeid)
 
-
+        logger.info("connecting to {} {}".format(mysql_host, mysql_db))
         # check if credentials are already in database
+        db = Mysql( host=mysql_host,
+                    user=mysql_user,
+                    passwd=mysql_passwd,
+                    db=mysql_db)
 
-        node_credentials = get_node_credentials(nodeid)
 
+
+        node_credentials = db.get_node_credentials(nodeid)
+        #print("node_credentials:", node_credentials, flush=True)
         if not node_credentials:
             try: 
-                generate_credentials(nodeid)
+                generate_credentials(db , nodeid)
             except Exception as e:
                 return "error: {}".format(str(e))
 
             try: 
-                node_credentials = get_node_credentials(nodeid)
+                node_credentials = db.get_node_credentials(nodeid)
             except Exception as e:
                 return "error: {}".format(str(e))
-                
+
         if not node_credentials:
             return "error: Could not create crdentials"
 
 
-        return '{key}\n{cert}\nTOKEN={token}\nPORT={ssh_port}\n{ssh_key}\n'.format(
+        mysql_row_node = db.get_node(nodeid)
+
+        if not mysql_row_node:
+            port=db.createNewNode(nodeid)
+            if not port:
+                print("Error: Node creation failed")
+                raise Exception("Node creation failed")
+            mysql_row_node = db.get_node(nodeid)
+
+        port = int(db.find_port(nodeid))
+
+        if not port:
+            logger.error("Error: port number not found !?")
+            raise Exception("port number not found !?")
+
+
+        #print("A", flush=True)
+        #print("node_credentials", node_credentials, flush=True)
+        try:
+            rsa_private_key = node_credentials['rsa_private_key']
+            rsa_public_key = node_credentials['rsa_public_key']
+            signed_client_certificate = node_credentials['signed_client_certificate']
+        except Exception as e:
+            return 'error: credential incomplete'
+        #print("rsa_private_key:", len(rsa_private_key), flush=True)
+        #print("rsa_public_key:", len(rsa_public_key), flush=True)
+        #print("signed_client_certificate:", len(signed_client_certificate), flush=True)
+
+
+        #print("port", port, flush=True)
+
+        #print("B", '{key}\n{cert}\nPORT={ssh_port}'.format(
+        #    key=rsa_private_key, cert=signed_client_certificate, ssh_port=port) , flush=True)
+        # removed TOKEN={token}\n
+        return_content = '{key}\n{cert}\nPORT={ssh_port}\n{ssh_key}\n'.format(
             key=rsa_private_key,
             cert=signed_client_certificate,
-            token=token,
             ssh_port=port,
             ssh_key=rsa_public_key)
+        
+        #print("return_content", return_content, flush=True)
+
+        #print("C", flush=True)
+
+        return return_content
 
 
 def update_authorized_keys_file():
@@ -220,36 +262,74 @@ def append_to_authorized_keys_file(data):
         file.write(data.strip())
         file.write('\n')
 
-    os.chmod(authorized_keys_file, 0600)
+    os.chmod(authorized_keys_file, 0o600)
 
 
 if __name__ == "__main__":
     node_database = {}
 
     # get all public keys from disk
-    for d in listdir(ssl_nodes_dir):
-        if isdir(join(ssl_nodes_dir, d)) and d[0:5] == 'node_':
-            rsa_pub_filename =  os.path.join(ssl_nodes_dir, d, 'key_rsa.pub')
+    for directory in listdir(ssl_nodes_dir):
+        node_dir = join(ssl_nodes_dir, directory)
+        if isdir(node_dir) and directory[0:5] == 'node_':
+            rsa_pub_filename =  os.path.join(node_dir, 'key_rsa.pub')
             try:
                 with open(rsa_pub_filename, 'r') as rsa_pub_file:
                     data=rsa_pub_file.read()
-                    node_id = d[5:].upper()
+                    node_id = directory[5:].upper()
                     node_database[node_id] = {}
                     node_database[node_id]['pub']=data
             except Exception as e:
                 logger.error("Error reading file %s: %s" % (rsa_pub_filename, str(e)))
 
-    print str(node_database)
+    print("node_database: (public keys only)")
+    print(str(node_database))
 
-    db = Mysql( host="beehive-mysql",
-                user="waggle",
-                passwd="waggle",
-                db="waggle")
+    db = Mysql( host=mysql_host,
+                    user=mysql_user,
+                    passwd=mysql_passwd,
+                    db=mysql_db)
+
+    # get list of nodes with credentials in MySQL
+    credentials_in_mysql = {}
+    for row in db.query_all('SELECT node_id FROM credentials'):
+        print(row)
+        node_id = row[0]
+        credentials_in_mysql[node_id]={'node_id': node_id}
+    
+    
+
+    # load credentials into MySQL (only used temporarily to move files into mysql)
+    for d in listdir(ssl_nodes_dir):
+        node_dir = join(ssl_nodes_dir, d)
+        if isdir(node_dir) and d[0:5] == 'node_':
+            node_id = d[5:].upper()
+            if node_id in credentials_in_mysql:
+                print("good, already in database")
+            else:
+                print("credentials missing in db! Trying to load into MYSQL ...")
+
+                rsa_public_key_file=os.path.join(node_dir, 'key_rsa.pub')
+                rsa_private_key_file=os.path.join(node_dir, 'key.pem')
+                signed_client_certificate_file=os.path.join(node_dir, 'cert.pem')
+
+                try:
+                    rsa_private_key = read_file(rsa_private_key_file)
+                    rsa_public_key = read_file(rsa_public_key_file)
+                    signed_client_certificate = read_file(signed_client_certificate_file)
+                except OSError as e:
+                    sys.exit('Could not read credential files: {}'.format(str(e)))
+
+                try:
+                    db.save_node_credentials(node_id, rsa_private_key, rsa_public_key, signed_client_certificate)
+                except Exception as e:
+                    sys.exit('Could not save credentials to MySQL database: {}'.format(str(e)))
+
 
     # get port: for node_id SELECT reverse_ssh_port FROM nodes WHERE node_id='0000001e06200335';
     # get nodes and ports from database
     for row in db.query_all('SELECT node_id, reverse_ssh_port FROM nodes'):
-        print row
+        print(row)
 
         node_id = row[0].upper()
 
@@ -273,6 +353,7 @@ if __name__ == "__main__":
         if not 'reverse_ssh_port' in node_database[node_id]:
             logger.warning("Node %s has public key, but no port number is assigned in database." % (node_id))
 
+    print("node_database:")
     pp = pprint.PrettyPrinter(indent=4)
     pp.pprint(node_database)
 
@@ -298,6 +379,8 @@ if __name__ == "__main__":
                 permitopen = 'permitopen="localhost:%d"' % (port)
                 line="%s,%s %s node:%s\n" % (permitopen, auth_options, node_database[node_id]['pub'].strip(), node_id)
             else:
+
+                logger.warning("Node %s has no reverse_ssh_port" % (node_id))
                 # add public keys without port number, but comment the line
                 permitopen = 'permitopen="localhost:?"'
                 line="#%s,%s %s node:%s\n" % (permitopen, auth_options, node_database[node_id]['pub'].strip(), node_id)
@@ -312,7 +395,8 @@ if __name__ == "__main__":
     with open(authorized_keys_file, 'w') as file:
         file.writelines(new_authorized_keys_content)
 
-    os.chmod(authorized_keys_file, 0600)
+    os.chmod(authorized_keys_file, 0o600)
 
+    web.config.debug = False
     web.httpserver.runsimple(app.wsgifunc(), ("0.0.0.0", httpserver_port))
     app.run()
