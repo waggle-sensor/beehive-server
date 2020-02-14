6ef9d9503265e316444149f3eea179c2eb22160f
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-02-14 13:18:10 -0600

    added note about rm step

--- redeploy.sh
+++ redeploy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-export BEEHIVE_ROOT=$PWD/beehive-root/
+export BEEHIVE_ROOT=$PWD/beehive-root
 
 ./do.sh cleanup
+
+# BE VERY CAREFUL ABOUT CHANGING THIS rm!
+# You don't want to allow this to wipe a sensitive directory!
 rm -rf $BEEHIVE_ROOT
+
 sleep 10
 ./do.sh deploy
 sleep 30
