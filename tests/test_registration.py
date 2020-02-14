2d598a3eed97c1da2b54c60643d4d20e51d7cbc6
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-02-14 13:35:48 -0600

    converted to standard unittest

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,29 +1,38 @@
+import unittest
 import requests
 import subprocess
 import time
 
-nodeid = '0000000000000001'
 
-# submit request for credentials
-r = requests.post(f'http://localhost/api/registration?nodeid={nodeid}').json()
-requestid = r['data']
+class TestRegistration(unittest.TestCase):
 
-# approve request
-# TODO replace with what will become approval api
-subprocess.check_output([
-    'docker', 'exec', '-ti', 'beehive-mysql', 'mysql',
-    '-u', 'waggle',
-    '--password=waggle',
-    '-D', 'waggle',
-    '-e', f"update registrations set state='approved', response_date=NOW() where id='{requestid}';",
-])
+    # TODO can make more fine grain. just moved end-to-end into single test for now
 
-# print credentials
-r = requests.get(f'http://localhost/api/registration/{requestid}')
-output = r.text
-print(output)
+    def test_full(self):
+        nodeid = '0000000000000001'
 
-assert 'RSA PRIVATE KEY' in output
-assert 'CERTIFICATE' in output
-assert 'PORT' in output
-assert 'ssh-rsa' in output
+        # submit request for credentials
+        r = requests.post(
+            f'http://localhost/api/registration?nodeid={nodeid}').json()
+        requestid = r['data']
+
+        # approve request
+        # TODO replace with what will become approval api
+        subprocess.check_output([
+            'docker', 'exec', '-ti', 'beehive-mysql', 'mysql',
+            '-u', 'waggle',
+            '--password=waggle',
+            '-D', 'waggle',
+            '-e', f"update registrations set state='approved', response_date=NOW() where id='{requestid}';",
+        ])
+
+        # print credentials
+        r = requests.get(f'http://localhost/api/registration/{requestid}')
+        self.assertIn('RSA PRIVATE KEY', r.text)
+        self.assertIn('CERTIFICATE', r.text)
+        self.assertIn('PORT', r.text)
+        self.assertIn('ssh-rsa', r.text)
+
+
+if __name__ == '__main__':
+    unittest.main()
