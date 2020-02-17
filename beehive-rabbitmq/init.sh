196f96e9cc6939609221e7bcd0d3335cb154e7a7
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2020-02-17 12:44:19 -0600

    fix case of empto rabbitmq

--- beehive-rabbitmq/init.sh
+++ beehive-rabbitmq/init.sh
@@ -1,8 +1,12 @@
-
-#!bin/bash
+#!/bin/bash
 
 set -e
-#set -x
+
+
+# This script creates users in RabbitMQ.
+# It is a bit complicated because rabbitmqctl exits with an error if a user already exists.
+# To avoid this we get a list of all existing users in RabbitMQ and create only those missing. 
+
 
 
 if [ $# -lt 1 ]; then
@@ -15,7 +19,7 @@ fi
 
 
 if [ -z ${RABBITMQ_ADMIN_PASSWORD} ] ; then
-	echo "Varibale RABBITMQ_ADMIN_PASSWORD not set"
+	echo "Variable RABBITMQ_ADMIN_PASSWORD not set"
 	exit 1
 fi
 
@@ -24,14 +28,25 @@ fi
 
 container_name=${1}
 
-echo name=${name}
+
+
+if [ -z ${container_name} ] ; then
+	echo "Variable container_name not set"
+	exit 1
+fi
+
+
 
 # wait for rabbitmq to up
 docker exec -ti ${container_name} rabbitmqctl status > /dev/null
 
+echo "Getting list of RabbitMQ users..."
+unset RABBIT_USERS
+set -x
+RABBIT_USERS=$( for i in $(docker exec -ti ${container_name} rabbitmqctl list_users -q) ; do echo $i  ; done | grep -v "^\[" || true )
+set +x
 
-RABBIT_USERS=$( for i in $(docker exec -ti ${container_name} rabbitmqctl list_users -q) ; do echo $i  ; done | grep -v "^\[" )
-
+echo "Creating missing RabbitMQ users..."
 # \b is matches word boundary
 
 if [ $(echo ${RABBIT_USERS} | grep "\badmin" | wc -l | tr -d " ") -eq 0 ] ; then
@@ -40,6 +55,8 @@ if [ $(echo ${RABBIT_USERS} | grep "\badmin" | wc -l | tr -d " ") -eq 0 ] ; then
 	docker exec -ti ${container_name} rabbitmqctl set_user_tags admin administrator
 	docker exec -ti ${container_name} rabbitmqctl set_permissions admin ".*" ".*" ".*"
 	set +x
+else
+	echo "RabbitMQ user admin already exists"
 fi
 
 if [ $(echo ${RABBIT_USERS} | grep "\bnode" | wc -l | tr -d " ") -eq 0 ] ; then
@@ -47,6 +64,8 @@ if [ $(echo ${RABBIT_USERS} | grep "\bnode" | wc -l | tr -d " ") -eq 0 ] ; then
 	docker exec -ti ${container_name} rabbitmqctl add_user node waggle
 	docker exec -ti ${container_name} rabbitmqctl set_permissions node "pull-images" ".*" "pull-images"
 	set +x
+else
+	echo "RabbitMQ user node already exists"
 fi
 
 
@@ -56,6 +75,8 @@ if [ $(echo ${RABBIT_USERS} | grep "\bserver" | wc -l | tr -d " ") -eq 0 ] ; the
 	docker exec -ti ${container_name} rabbitmqctl add_user server waggle
 	docker exec -ti ${container_name} rabbitmqctl set_permissions server ".*" ".*" ".*"
 	set +x
+else
+	echo "RabbitMQ user server already exists"
 fi
 
 
@@ -65,6 +86,8 @@ if [ $(echo ${RABBIT_USERS} | grep "\brouter" | wc -l | tr -d " ") -eq 0 ] ; the
 	docker exec -ti ${container_name} rabbitmqctl add_user router router
 	docker exec -ti ${container_name} rabbitmqctl set_permissions router ".*" ".*" ".*"
 	set +x
+else
+	echo "RabbitMQ user router already exists"
 fi
 
 
@@ -74,4 +97,6 @@ if [ $(echo ${RABBIT_USERS} | grep "\bloader_raw" | wc -l | tr -d " ") -eq 0 ] ;
 	docker exec -ti ${container_name} rabbitmqctl add_user loader_raw waggle
 	docker exec -ti ${container_name} rabbitmqctl set_permissions loader_raw "^db-raw$" "^$" "^db-raw$"
 	set +x
+else
+	echo "RabbitMQ user loader_raw already exists"
 fi
