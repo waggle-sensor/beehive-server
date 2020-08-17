5503a012597b2e3a6b99709cf492793b16a20fd8
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-08-17 13:50:59 -0500

    change #!/bin/sh to #!/bin/bash in do.sh

--- do.sh
+++ do.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # ANL:waggle-license
 #  This file is part of the Waggle Platform.  Please see the file
 #  LICENSE.waggle.txt for the legal details of the copyright and software
